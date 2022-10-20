b036b41649364626de0d07e52fab49aa0dfdcf0c
leoarthurwall <leowall93@gmail.com>
2022-10-21 00:19:16 +0100

    added box sizing: border box to contain the navbar with it's padding within the image dive, without overflow

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,32 +1,45 @@
 import styled from "styled-components";
+import { MagnifyingGlassIcon, Bars3Icon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
-
-    const NavbarContainer = styled.div`
+  const NavbarContainer = styled.div`
     position: absolute;
     top: 0;
     display: flex;
-    justify-content: space between;
-    align-items; center;
+    justify-content: space-between;
+    align-items: center;
     width: 100%;
     padding: 2rem 2rem;
-    `
-    const Left = styled.div`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    `
-    const Name = styled.h2`
+    box-sizing: border-box;
+
+    `;
+  const Left = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+  const Right = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+  const Name = styled.h2`
     color: black;
     margin: 0;
-    `
+  `;
   return (
     <NavbarContainer>
-        <Left>
-            <Name>Pearson Lloyd</Name>
-        </Left>
-    </NavbarContainer>
-  )
-}
+      <Left>
+        <Name>Pearson Lloyd</Name>
+        <MagnifyingGlassIcon style={{color: "white", fontSize: "1rem"}}/>
+      </Left>
+      <Right>
+      <Name>Pearson Lloyd</Name>
 
-export default Navbar
+        <Bars3Icon />
+      </Right>
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
--- src/Components/SectionOne/SectionOne.js
+++ src/Components/SectionOne/SectionOne.js
@@ -11,6 +11,7 @@ const SectionOne = () => {
     display: flex;
     justify-content: center;
     align-items: center;
+
 `;
 
   const Header = styled.h1`
