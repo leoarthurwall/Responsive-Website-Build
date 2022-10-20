38e2f71027a3921504fee9d615db42a5fca9d65a
leoarthurwall <leowall93@gmail.com>
2022-10-20 23:11:36 +0100

    imported roboto google text as unable to find matching font, centered it on the screen using position relative and absolute

--- src/Components/SectionOne/SectionOne.js
+++ src/Components/SectionOne/SectionOne.js
@@ -6,11 +6,25 @@ const SectionOne = () => {
   const SectionOneImage = styled.div`
     height: 100vh;
     width: 100wv;
-    background-image:url('https://pearsonlloyd.com/wp-content/uploads/2022/09/HOMEPAGE-6.jpg')
+    background-image: url("https://pearsonlloyd.com/wp-content/uploads/2022/09/HOMEPAGE-6.jpg");
+    position: relative;
   `;
 
+  const Header = styled.h1`
+  width: 100%;
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    font-family: roboto;
+    font-weight: 700;
+    font-size: 7.6rem;
+  `;
   return (
-    <SectionOneImage />
+    <SectionOneImage>
+      <Header>Design for Change</Header>
+    </SectionOneImage>
   );
 };
 
