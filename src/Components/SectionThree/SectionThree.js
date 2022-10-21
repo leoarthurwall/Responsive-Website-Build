2d8bf393b5e422e8d633706e57ae9191a4aae4d4
leoarthurwall <leowall93@gmail.com>
2022-10-22 00:10:11 +0100

    create an object with all of the projects. client, name, url

--- src/Components/SectionThree/SectionThree.js
+++ src/Components/SectionThree/SectionThree.js
@@ -9,21 +9,52 @@ const ProjectContainer = styled.div`
   justify-content: center;
   align-items: center;
   overflow: hidden;
+  margin-top: 15px;
 `;
 
 const SectionThree = () => {
   const Project = [
     {
-      Name: "one",
+      Client: "Modus",
+      Name: "Edge Free",
       bgImage:
         "https://pearsonlloyd.com/wp-content/uploads/2022/10/EDGE-FREE-TILE-1540X1236.jpg",
+    },
+    {
+      Client: "LDF 2022",
+      Name: "Change Making",
+      bgImage:
+        "https://pearsonlloyd.com/wp-content/uploads/2022/08/AVRO-TILE.png",
+    },
+    {
+      Client: "Profim",
+      Name: "Revo",
+      bgImage:
+        "https://pearsonlloyd.com/wp-content/uploads/2022/05/PROJECT-TILE-TEMPLATE-1540X1236-2.jpg",
+    },
+    {
+      Client: "Arconas",
+      Name: "Avro",
+      bgImage:
+        "https://pearsonlloyd.com/wp-content/uploads/2022/08/AVRO-TILE.png",
+    },
+    {
+      Client: "Bene",
+      Name: "bFRIENDS",
+      bgImage:
+        "https://pearsonlloyd.com/wp-content/uploads/2021/11/PROJECT-TILE-TEMPLATE-1540X1236.jpg",
     },
   ];
 
   return (
-    <ProjectContainer
-      style={{ backgroundImage: `url(${Project[0].bgImage})` }}
-    ></ProjectContainer>
+    <>
+      <ProjectContainer
+        style={{ backgroundImage: `url(${Project[0].bgImage})` }}
+      ></ProjectContainer>
+      <ProjectContainer
+        style={{ backgroundImage: `url(${Project[1].bgImage})` }}
+      ></ProjectContainer>
+    </>
   );
 };
 
