b66c952fa849553f2231be72582eb0434e4cf67d
leoarthurwall <leowall93@gmail.com>
2022-10-22 12:04:36 +0100

    added sub containers to the footer and gave them padding. will wrap them in another container and give margin instead to make more responsive hopefully

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,18 +1,47 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const FooterContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #9bad97;
-    height: 50px;
-    width: 100vw;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #9bad97;
+  height: 50px;
+  width: 100vw;
+  padding: 0 7rem;
+  box-sizing: border-box;
+
+`;
+const FooterSubContainer = styled.div`
+  display: flex;
+  width: 100%;
+
+
+  justify-content: space-between;
+  align-items: center;
+  text-align: left;
+`;
+const FooterText = styled.p`
+  font-family: inter;
+  color: #fff;
+`;
 
 const Footer = () => {
   return (
-    <FooterContainer>Footer</FooterContainer>
-  )
-}
+    <FooterContainer>
+      <FooterSubContainer>
+        <FooterText>FooterText</FooterText>
+      </FooterSubContainer>
+      <FooterSubContainer>
+        <FooterText>FooterText</FooterText>
+      </FooterSubContainer>
+      <FooterSubContainer>
+        <FooterText>FooterText</FooterText>
+      </FooterSubContainer>
+      <FooterSubContainer>
+        <FooterText>FooterText</FooterText>
+      </FooterSubContainer>
+    </FooterContainer>
+  );
+};
 
-export default Footer
+export default Footer;
