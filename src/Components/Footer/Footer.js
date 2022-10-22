e6256424643ad29b7f2556e94a5197bdac6d8e36
leoarthurwall <leowall93@gmail.com>
2022-10-22 12:43:03 +0100

    provided padding around footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,23 +3,24 @@ import styled from "styled-components";
 const FooterContainer = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
   background-color: #9bad97;
   width: 100vw;
-  padding: 0 10rem;
+  padding: 37px 10rem 133px 10rem;
   box-sizing: border-box;
+
 `;
 const FooterSubContainer = styled.div`
   display: flex;
   width: 100%;
 
   justify-content: space-between;
-  align-items: center;
   text-align: left;
 `;
 const FooterText = styled.p`
   font-family: inter;
   color: #fff;
+  margin: 0;
+
 `;
 
 const UList = styled.ul`
