d2553d4a0b66f817533fa777ade225251c77cfa2
leoarthurwall <leowall93@gmail.com>
2022-10-22 21:08:20 +0100

    SOLVEDgit add .git add . I had the styled components within the component function so they weren't transitioning. onced taken outside of the component function, the transition works as normal! amazing :)

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <>
-      <SectionOne />
       <Navbar />
+      <SectionOne />
       <SectionTwo />
       <SectionThree />
       <Footer />
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,73 +2,72 @@ import styled from "styled-components";
 import { MagnifyingGlassIcon, Bars3Icon } from "@heroicons/react/24/solid";
 import { useState, useEffect } from "react";
 
+const NavbarContainer = styled.div`
+  position: fixed;
+  z-index: 50;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 2.5rem 2rem;
+  box-sizing: border-box;
+  transition: 1s ease;
+
+  &.active {
+    background-color: white;
+    padding: 1rem 2rem;
+  }
+`;
+
+const Left = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Right = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+const Name = styled.h2`
+  color: black;
+  margin: 0;
+  font-family: inter;
+`;
+
 const Navbar = () => {
-  const [scrollOffset, setScrollOffset] = useState(false);
+  const [offset, setOffset] = useState(0);
+  const [isUpper, setIsUpper] = useState(false);
 
-  const Nav = styled.nav`
-    width: 100%;
-  `;
-  const NavbarContainer = styled.div`
-    position: fixed;
-    top: 0;
-    z-index: 50;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    padding: 2.5rem 2rem;
-    box-sizing: border-box;
-    transition: 1s ease;
-    &.active {
-      background-color: white;
-      padding: 1rem 2rem;
-    }
-  `;
+  const handleScroll = () => setOffset(window.pageYOffset);
 
-  const Left = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-  const Right = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-  `;
-  const Name = styled.h2`
-    color: black;
-    margin: 0;
-    font-family: inter;
-  `;
-
-  const changeBackground = () => {
-    console.log(window.scrollY);
-    if (window.scrollY > 0) {
-      setScrollOffset(true);
-    } else {
-      setScrollOffset(false);
-    }
-  };
   useEffect(() => {
-    changeBackground();
-    // adding the event when scroll change background
-    window.addEventListener("scroll", changeBackground);
-  });
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (offset > 0) {
+      setIsUpper(true);
+    } else {
+      setIsUpper(false);
+    }
+  }, [offset]);
+
 
   return (
-    <Nav>
-      <NavbarContainer className={scrollOffset ? "active" : ""}>
-
-        <Left>
-          <Name>Pearson Lloyd</Name>
-        </Left>
-        <Right>
-          <MagnifyingGlassIcon style={{ height: "40px", width: "40px" }} />
-          <Bars3Icon style={{ height: "40px", width: "40px" }} />
-        </Right>
-      </NavbarContainer>
-    </Nav>
+    <NavbarContainer className={isUpper ? "active" : ""}>
+      <Left>
+        <Name>Pearson Lloyd</Name>
+      </Left>
+      <Right>
+        <MagnifyingGlassIcon style={{ height: "40px", width: "40px" }} />
+        <Bars3Icon style={{ height: "40px", width: "40px" }} />
+      </Right>
+    </NavbarContainer>
   );
 };
 
