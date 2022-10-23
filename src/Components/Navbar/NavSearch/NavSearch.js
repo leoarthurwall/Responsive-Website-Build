d3332005bae52e1d9d86897af391c9ae41ea6c38
leoarthurwall <leowall93@gmail.com>
2022-10-23 12:34:33 +0100

    created a NavSearch component and gave it a container div - will attempt to render to make sure its working

---
+++ src/Components/Navbar/NavSearch/NavSearch.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+const SearchContainer = styled.div`
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: 40;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transition: all 1s ease-in;
+  &.active {
+    background-color: #9bad97;
+  }
+`;
+const NavSearch = () => {
+  return (
+    <SearchContainer>Search</SearchContainer>
+  )
+}
+
+export default NavSearch
