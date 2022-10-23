2c8a139b15ca940fb6b54aac7803604396406135
leoarthurwall <leowall93@gmail.com>
2022-10-23 13:02:07 +0100

    styled the search input with placeholder styling too

--- src/Components/Navbar/NavSearch/NavSearch.js
+++ src/Components/Navbar/NavSearch/NavSearch.js
@@ -1,4 +1,4 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const SearchContainer = styled.div`
   position: fixed;
@@ -15,10 +15,34 @@ const SearchContainer = styled.div`
     background-color: #9bad97;
   }
 `;
+
+const SearchBar = styled.input`
+  background-color: rgba(0, 0, 0, 0) !important;
+  border: none !important;
+  border-bottom: 1px solid #fff !important;
+  box-shadow: none !important;
+  height: 80px;
+  font-size: 62px;
+  font-weight: 600;
+  color: #fff;
+  padding: 0 15px;
+  max-width: 935px;
+
+  &:focus {
+    outline: none;
+    border: none !important;
+  }
+  ::placeholder {
+    color: #fff;
+  }
+`;
+
 const NavSearch = (isSearchClicked) => {
   return (
-    <SearchContainer className={isSearchClicked ? "active" : ""}>Search</SearchContainer>
-  )
-}
+    <SearchContainer className={isSearchClicked ? "active" : ""}>
+      <SearchBar type="text" placeholder="Search" />
+    </SearchContainer>
+  );
+};
 
-export default NavSearch
+export default NavSearch;
